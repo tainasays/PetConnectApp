e155ba4e2c60abe60645c082c11ff6526237b821
tainasays <thaynasant.ana@hotmail.com>
2026-05-05 23:32:36 -0300

    prática 1 - LoginActivity

---
+++ app/src/main/java/com/example/petconnectapp/LoginActivity.kt
@@ -0,0 +1,100 @@
+package com.example.petconnectapp
+import androidx.compose.ui.platform.LocalContext
+import android.app.Activity
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.petconnectapp.ui.theme.PetConnectAppTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            PetConnectAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val activity = LocalContext.current as Activity
+
+    val fieldModifier = Modifier.fillMaxWidth(0.9f)
+    Column(
+        modifier = modifier.padding(24.dp).fillMaxSize(),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = modifier.size(12.dp))
+
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = fieldModifier,
+            onValueChange = { email = it }
+        )
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = fieldModifier,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = modifier.size(12.dp))
+
+
+        Row(
+
+            modifier = fieldModifier,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button( onClick = {
+                Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+            },
+                enabled = email.isNotEmpty() && password.isNotEmpty()
+             ) {
+                Text("Login")
+            }
+            Button(
+                onClick = { email = ""; password = "" }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+
+
+}
+
+
