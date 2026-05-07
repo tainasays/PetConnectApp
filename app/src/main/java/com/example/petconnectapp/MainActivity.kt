d6a3a4a742f573f95a196dba70afce0d74722aa3
tainasays <thaynasant.ana@hotmail.com>
2026-05-06 21:21:14 -0300

    prática 2: parte 1

--- app/src/main/java/com/example/petconnectapp/MainActivity.kt
+++ app/src/main/java/com/example/petconnectapp/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.petconnectapp.ui.theme.HomePage
+import com.example.petconnectapp.ui.HomePage
 import com.example.petconnectapp.ui.theme.PetConnectAppTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/petconnectapp/ui/HomePage.kt
+++ app/src/main/java/com/example/petconnectapp/ui/HomePage.kt
@@ -1,6 +1,5 @@
-package com.example.petconnectapp.ui.theme
+package com.example.petconnectapp.ui
 
-import android.app.Activity
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.foundation.layout.wrapContentSize
@@ -8,19 +7,13 @@ import androidx.compose.foundation.background
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
