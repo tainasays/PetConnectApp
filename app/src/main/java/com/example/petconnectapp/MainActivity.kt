4339bf8973e06078777aaef630afb47a1cd3bdca
tainasays <thaynasant.ana@hotmail.com>
2026-05-06 21:12:11 -0300

    prática 2: parte 1

--- app/src/main/java/com/example/petconnectapp/MainActivity.kt
+++ app/src/main/java/com/example/petconnectapp/MainActivity.kt
@@ -1,27 +1,14 @@
 package com.example.petconnectapp
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.petconnectapp.ui.theme.HomePage
 import com.example.petconnectapp.ui.theme.PetConnectAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,29 +21,6 @@ class MainActivity : ComponentActivity() {
                     HomePage(modifier = Modifier.padding(innerPadding))
                 }
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as Activity
-    val fieldModifier = modifier.fillMaxSize()
-
-    Column(
-        modifier = fieldModifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Hello, world!")
-
-        Spacer(modifier = Modifier.size(12.dp))
-
-        Button(
-            onClick = { activity.finish() }
-        ) {
-            Text("Sair")
         }
     }
 }
